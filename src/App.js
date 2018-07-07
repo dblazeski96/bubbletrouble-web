23ece653ada1d4ec4e94c7169d70e3911f000ef1
Jovan Kanevche <kanevche.jovan@gmail.com>
2018-07-07 20:24:10 +0200

    stuf

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import ImportantProjects from './screens/ImportantProjects'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import {
+  faCheckSquare,
+  faCoffee,
+  faHospital,
+  faBroadcastTower,
+  faHandHoldingUsd,
+  faBuilding,
+  faBus,
+  faLongArrowAltUp,
+  faHandsHelping,
+  faBriefcase
+} from '@fortawesome/free-solid-svg-icons'
+import Plan from './component/Plan'
+import Hints from './screens/Hints'
+import Comparation from './screens/Comparation'
 
+library.add(
+  fab,
+  faCheckSquare,
+  faCoffee,
+  faHospital,
+  faBroadcastTower,
+  faHandHoldingUsd,
+  faBuilding,
+  faBus,
+  faLongArrowAltUp,
+  faHandsHelping,
+  faBriefcase
+)
 class App extends Component {
   render() {
     return (
       <div>
+        <Comparation />
+        <Hints />
+        <ImportantProjects />
 
+        <Plan />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/dummyData.js
+++ src/dummyData.js
@@ -1,61 +1,101 @@
 export default {
-    income: {
-      name: 'Вкупни Приходи',
-      value: 217700000
+  income: {
+    name: 'Вкупни Приходи',
+    value: 217700000
+  },
+  expenses: {
+    name: 'Вкупни Расходи',
+    value: 193500000
+  },
+  deficit: {
+    name: 'Дефицит',
+    value: 18200000
+  },
+  incomeData: [
+    { name: 'Даноци', value: 114 },
+    { name: 'Социјални придонеси', value: 55 },
+    { name: 'Неданочни приходи', value: 17 },
+    { name: 'Капитални Приходи и друго', value: 6.1 },
+    { name: 'Дефицит', value: 18200000 }
+  ],
+  expensesData: [
+    { name: 'Пензии и социјално', value: 75 },
+    { name: 'Економски Развој', value: 43 },
+    { name: 'Здраство', value: 32 },
+    {
+      name: 'Образование, наука и спорт',
+      value: 26
     },
-    expenses: {
-      name: 'Вкупни Расходи',
-      value: 193500000
+    {
+      name: 'Јавен ред',
+      value: 12
     },
-    deficit: {
-      name: 'Дефицит',
-      value: 18200000
+    { name: 'Градежништво и транспорт', value: 10 },
+    { name: 'Земјоделство', value: 10 },
+    { name: 'Одбрана', value: 7 },
+    {
+      name: 'Надворешни работи',
+      value: 5
     },
-    incomeData: [
-      { name: 'Даноци', value: 114 },
-      { name: 'Социјални придонеси', value: 55 },
-      { name: 'Неданочни приходи', value: 17 },
-      { name: 'Капитални Приходи и друго', value: 6.1 },
-      { name: 'Дефицит', value: 18200000 }
-    ],
-    expensesData: [
-      { name: 'Пензии и социјално', value: 75 },
-      { name: 'Економски Развој', value: 43 },
-      { name: 'Здраство', value: 32 },
-      {
-        name: 'Образование, наука и спорт',
-        value: 26
-      },
-      {
-        name: 'Јавен ред',
-        value: 12
-      },
-      { name: 'Градежништво и транспорт', value: 10 },
-      { name: 'Земјоделство', value: 10 },
-      { name: 'Одбрана', value: 7 },
-      {
-        name: 'Надворешни работи',
-        value: 5
-      },
-      {
-        name: 'Култура',
-        value: 4
-      },
-      {
-        name: 'Регионален развој',
-        value: 2
-      },
-      { name: 'Судство', value: 2 }
-    ],
-    planMessage: 'Планирај со нас',
-    importantProjects: [
-      { name: 'Инвестиции во Здраство', value: 1.9 },
-      { name: 'Радиодифузна дејност', value: 1.2 },
-      { name: 'Субвенции', value: 8.4 },
-      { name: 'Подршка на домашни и странски компании', value: 2 },
-      { name: 'Транспортна и енергетска инфраструктура', value: 9.5 },
-      { name: 'Регионален Развој', value: 100 },
-      { name: 'Помош за исплата на минимални плати', value: 461 },
-      { name: 'Мерки за вработување', value: 988 }
-    ]
-  }
+    {
+      name: 'Култура',
+      value: 4
+    },
+    {
+      name: 'Регионален развој',
+      value: 2
+    },
+    { name: 'Судство', value: 2 }
+  ],
+  planMessage: 'Планирај со нас',
+  importantProjects: [
+    {
+      name: 'Инвестиции во Здраство',
+      value: 1.9,
+      icon: 'hospital',
+      color: '#9b59b6'
+    },
+    {
+      name: 'Радиодифузна дејност',
+      value: 1.2,
+      icon: 'broadcast-tower',
+      color: '#2ecc71'
+    },
+    {
+      name: 'Субвенции',
+      value: 8.4,
+      icon: 'hand-holding-usd',
+      color: '#2ecc71'
+    },
+    {
+      name: 'Подршка на домашни и странски компании',
+      value: 2,
+      icon: 'building',
+      color: '#f1c40f'
+    },
+    {
+      name: 'Транспортна и енергетска инфраструктура',
+      value: 9.5,
+      icon: 'bus',
+      color: '#e74c3c'
+    },
+    {
+      name: 'Регионален Развој',
+      value: 100,
+      icon: 'long-arrow-alt-up',
+      color: '#3498db'
+    },
+    {
+      name: 'Помош за исплата на минимални плати',
+      value: 461,
+      icon: 'hands-helping',
+      color: '#95a5a6'
+    },
+    {
+      name: 'Мерки за вработување',
+      value: 988,
+      icon: 'briefcase',
+      color: '#e67e22'
+    }
+  ]
+}
